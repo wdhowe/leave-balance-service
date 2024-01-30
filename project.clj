5ed7d68c8f78df28cae69e7ad5d4d11b886ccf06
Bill Howe <wdhowe@protonmail.com>
2024-01-29 18:01:31 -0600

    Project cleanup. (project.clj and github actions)

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject leave-balance-service "0.2.1"
+  
+  ;;; Project Metadata
   :description "HTTP service that calculates end of year leave/pto balance."
   :min-lein-version "2.0.0"
+
+  ;;; Dependencies, Plugins
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "1.1.0"]
                  [clojure.java-time "0.3.2"]
@@ -11,9 +15,12 @@
                  [ring/ring-json "0.5.0"]
                  [trptcolin/versioneer "0.2.0"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler leave-balance-service.handler/app}
-  :main ^:skip-aot leave-balance-service.handler
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+
+  ;;; Profiles
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}
-   :uberjar {:aot :all}})
+             :uberjar {:aot :all}}
+  
+  ;;; Running Project Code
+  :ring {:handler leave-balance-service.handler/app}
+  :main ^:skip-aot leave-balance-service.handler)
